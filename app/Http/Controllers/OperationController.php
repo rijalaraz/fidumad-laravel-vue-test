a94175d0fea34363e82ffdf39dde82f36c16b54d
Rija <rijalaraz@gmail.com>
2021-03-01 13:36:15 +0300

    First commit

---
+++ app/Http/Controllers/OperationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class OperationController extends Controller
+{
+    const API_URL = 'https://agrcf.lib.id/exercice@dev/';
+    
+    public function getList(Request $request)
+    {
+        $response = Http::get(self::API_URL);
+        $operations = $response->json()['operations'];
+        return response()->json($operations);
+    }
+}
